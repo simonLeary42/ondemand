cfd345d694520a54d48674f5fded29adbb70255c
Simon Leary <simon.leary42@proton.me>
2026-03-09 16:34:37 -0400

    fix variable names

--- apps/dashboard/app/javascript/dynamic_forms.js
+++ apps/dashboard/app/javascript/dynamic_forms.js
@@ -827,8 +827,8 @@ function tokenFromExclusiveOptionFor(str) {
   return sharedTokenFromOptionFor(str, 'exclusiveOptionFor');
 }
 
-function sharedToggleOptionsFor(_event, causeId, optionForType) {
-  const options = [...document.querySelectorAll(`#${causeId} option`)];
+function sharedToggleOptionsFor(_event, targetId, optionForType) {
+  const options = [...document.querySelectorAll(`#${targetId} option`)];
   let hideSelectedValue = undefined;
 
   options.forEach(option => {
@@ -845,18 +845,18 @@ function sharedToggleOptionsFor(_event, causeId, optionForType) {
       } else if (optionForType == 'exclusiveOptionFor') {
         token = tokenFromExclusiveOptionFor(key);
       }
-      let optionForId = idFromToken(key.replace(new RegExp(`^${optionForType}`),''));
+      let causeId = idFromToken(key.replace(new RegExp(`^${optionForType}`),''));
 
       // it's some other directive type, so just keep going and/or not real
-      if(!key.startsWith(optionForType) || optionForId === undefined) {
+      if(!key.startsWith(optionForType) || targetId === undefined) {
         continue;
       }
-      const value = document.getElementById(optionForId).value;
-      let causeValue = mountainCaseWords(value);
+      const causeValueRaw = document.getElementById(causeId).value;
+      let causeValue = mountainCaseWords(causeValueRaw);
 
-      let causeIdAlias = '';
-      if ((causeId in aliasLookup) && (value in aliasLookup[causeId])) {
-        causeIdAlias = aliasLookup[causeId][value];
+      let targetIdAlias = '';
+      if ((targetId in aliasLookup) && (causeValueRaw in aliasLookup[targetId])) {
+        targetIdAlias = aliasLookup[targetId][causeValueRaw];
       }
       // handle special case where the very first token here is a number.
       // browsers expect a prefix of hyphens as if it's the next token.
@@ -866,13 +866,13 @@ function sharedToggleOptionsFor(_event, causeId, optionForType) {
       if (optionForType == 'optionFor') {
         let key = `optionFor${token}${causeValue}`;
         if (!(key in option.dataset)) {
-          key = `optionFor${token}${causeIdAlias}`;
+          key = `optionFor${token}${targetIdAlias}`;
         }
         hide = option.dataset[key] === 'false';
       } else if (optionForType == 'exclusiveOptionFor') {
         let key = `exclusiveOptionFor${token}${causeValue}`;
         if (!(key in option.dataset)){
-          key = `exclusiveOptionFor${token}${causeIdAlias}`;
+          key = `exclusiveOptionFor${token}${targetIdAlias}`;
         }
         hide = !(option.dataset[key] === 'true');
       }
@@ -881,7 +881,7 @@ function sharedToggleOptionsFor(_event, causeId, optionForType) {
       }
     };
 
-    const elementInfo = getWidgetInfo(causeId);
+    const elementInfo = getWidgetInfo(targetId);
     if(hide) {
       if(option.selected) {
         option.selected = false;
@@ -902,7 +902,7 @@ function sharedToggleOptionsFor(_event, causeId, optionForType) {
   // be the current selected value.
   // if you've hidden what _was_ selected.
   if(hideSelectedValue !== undefined) {
-    let others = [...document.querySelectorAll(`#${causeId} option[value='${hideSelectedValue}']`)];
+    let others = [...document.querySelectorAll(`#${targetId} option[value='${hideSelectedValue}']`)];
     let newSelectedOption = undefined;
 
     // You have hidden what _was_ selected, so try to find a duplicate option that is visible
@@ -917,7 +917,7 @@ function sharedToggleOptionsFor(_event, causeId, optionForType) {
 
     // no duplicates are visible, so just pick the first visible option
     if (newSelectedOption === undefined) {
-      others = document.querySelectorAll(`#${causeId} option`);
+      others = document.querySelectorAll(`#${targetId} option`);
       others.forEach(ele => {
         if(newSelectedOption === undefined && ele.style.display === '') {
           newSelectedOption = ele;
@@ -931,7 +931,7 @@ function sharedToggleOptionsFor(_event, causeId, optionForType) {
   }
 
   // now that we're done, propagate this change to data-set or data-hide handlers
-  document.getElementById(causeId).dispatchEvent((new Event('change', { bubbles: true })));
+  document.getElementById(targetId).dispatchEvent((new Event('change', { bubbles: true })));
 }
 
 // get attributes based on widget id
